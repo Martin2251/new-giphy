e613470dc93c6a250d87b1baa4c4f238e6bbe973
martin lowe <martinlowe673@gmail.com>
2021-04-25 12:22:35 +0100

    Added api key

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Giphy = () => {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      axios("api.giphy.com/v1/gifs/trending", {
+        params: {
+          api_key: "bobN2UYDcCQoe3F3RKpP6kZ6GPnAfdu2"
+        }
+      })
+    }
+
+
+
+
   return <div className="">Giphy</div>;
 };
 
